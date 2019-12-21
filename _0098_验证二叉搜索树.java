a37a2afd674415189c6da72d64ff84efceb8bd46
Yankfu <857231252@qq.com>
2019-12-21 22:16:33 +0800

    深度优先搜索还是掌握的不够好

---
+++ _0098_验证二叉搜索树.java
@@ -0,0 +1,10 @@
+package com.LeetCode;
+
+public class _0098_验证二叉搜索树 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- _0104_二叉树的最大深度.java
+++ _0104_二叉树的最大深度.java
@@ -1,27 +1,27 @@
 package com.LeetCode;
 
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
-}
-
 public class _0104_二叉树的最大深度 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		TreeNode head = new TreeNode(3);
+		head.left=new TreeNode(9);
+		head.right=new TreeNode(20);
+		head.right.left=new TreeNode(15);
+		head.right.right=new TreeNode(7);
+		System.out.println(maxDepth(head));
 	}
 	
 	public static int maxDepth(TreeNode root) {
-        int reasult=0;
-        
-        
-        
-		return 0;
+        if(root==null) {
+        	return 0;
+        }else {
+        	int left=maxDepth(root.left);
+        	int right=maxDepth(root.right);
+        	return java.lang.Math.max(left, right) + 1;
+        }
     }
 	public static void count(TreeNode root,int count) {
-		
+
 	}
 }
