7894e9cf5d62c2b64adcc09c08f035cc9bcc748c
Yankfu <857231252@qq.com>
2019-12-28 22:27:37 +0800

    跟个咸鱼一样睡了一天

--- _0496_下一个更大元素I.java
+++ _0496_下一个更大元素I.java
@@ -6,8 +6,8 @@ public class _0496_下一个更大元素I{
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] nums1= {2,4};
-		int[] nums2= {1,2,3,4};
+		int[] nums1= {4,1,2};
+		int[] nums2= {1,3,4,2};
 		int[] nums3=nextGreaterElement(nums1,nums2);
 		for(int num:nums3)
 		{
@@ -16,49 +16,27 @@ public class _0496_下一个更大元素I{
 	}
 
 	public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
-		int[] num3=new int[nums1.length];
-		Stack<Integer> s=new Stack<>();
-		int k=0;
-		for(int i:nums2)
-		{
-			s.push(i);
-		}
-		//��һ��ѭ����¼�±�
-		for(int i:nums1)
-		{
-			for(int j=0;j<nums2.length;j++)
-			{
-				if(i==nums2[j])
-				{
-					num3[k++]=j;
+		int[] result = new int[nums1.length];
+		
+		for(int i=0;i<nums1.length;i++) {
+			int flag=0;
+			for(int j=0;j<nums2.length;j++) {
+				if(nums2[j]==nums1[i]) {
+					flag=1;
+				}else if(nums2[j]!=nums1[i] && flag==0) {
+					continue;
 				}
-			}
-		}
-		for(int i:num3)
-		{
-			System.out.print(i+" ");
-		}
-		System.out.println();
-		for(int i=0;i<num3.length;i++)
-		{
-			if(num3[i]+1==nums2.length)
-			{
-				num3[i]=-1;
-			}
-			for(int j=num3[i]+1;j<nums2.length;j++)
-			{
-				if(nums2[j]>nums2[num3[i]])
-				{
-					num3[i]=nums2[j];
+				if(nums2[j]>nums1[i] && flag==1) {
+					result[i]=nums2[j];
 					break;
 				}
-				if(j==nums2.length-1 && nums2[nums2.length-1]<nums2[num3[i]])
-				{
-					System.out.println(66666);
-					num3[i]=-1;
-				}
 			}
 		}
-		return num3;
+		for(int i=0;i<nums1.length;i++) {
+			if(result[i]==0) {
+				result[i]=-1;
+			}
+		}
+		return result;
     }
 }
--- _test.java
+++ _test.java
@@ -3,17 +3,7 @@ package com.LeetCode;
 public class _test {
 
 	public static void main(String[] args) {
-		try {
-			System.out.println("a");
-			int i=100/0;
-			System.out.println(i);
-		}catch(RuntimeException e) {
-			System.out.println("RuntimeException");
-		}catch(Exception e) {
-			System.out.println("Exception");
-		}finally {
-			System.out.println("Finally");
-		}
-		System.out.println("计算完应该会打印这里，不过给的答案不打印");
+		
 	}
 }
+    
