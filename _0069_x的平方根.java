4605cd5a6e952ed4a79071d4defda00a7b73c34b
Yankfu <857231252@qq.com>
2020-01-01 21:51:43 +0800

    元旦快乐

---
+++ _0069_x的平方根.java
@@ -0,0 +1,24 @@
+package com.LeetCode;
+
+public class _0069_x的平方根 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(mySqrt(Integer.MAX_VALUE));
+		System.out.println(Math.sqrt(Integer.MAX_VALUE));
+	}
+	public static int mySqrt(int x) {
+		if(x==Integer.MAX_VALUE) {
+			x=x-1;
+		}
+        double xn = x;
+        int a=(int)xn;
+        int b=Integer.MAX_VALUE;
+        while(a!=b) {
+        	xn=xn-(xn*xn-x)/(2*xn);
+        	b=a;
+        	a=(int)xn;
+        }
+		return a;
+    }
+}
