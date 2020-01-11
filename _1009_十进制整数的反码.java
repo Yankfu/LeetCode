d060a13edb0c853ca5b2a27bf10773754ac9aff1
Yankfu <857231252@qq.com>
2020-01-12 00:37:57 +0800

    锅被收了，qtmd

--- _1009_十进制整数的反码.java
+++ _1009_十进制整数的反码.java
@@ -4,29 +4,32 @@ public class _1009_十进制整数的反码 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(bitwiseComplement(5));
+		System.out.println(bitwiseComplement(1));
 	}
+
 	public static int bitwiseComplement(int N) {
-		int temp=0;
-		while(N!=0) {
-			if((N&1)==0) {
-				temp=temp|1;
-			}else {
-				
-			}
-			N=N>>>1;
-			temp=temp<<1;
+		if(N==0) {
+			return 1;
 		}
-		int res=0;
-		while(temp!=0) {
-			if((temp&1)==1) {
-				res=res|1;
-			}else {
-				
+		StringBuilder sb = new StringBuilder();
+		while (N != 0) {
+			if ((N & 1) == 0) {
+				sb.append("1");
+			} else {
+				sb.append("0");
 			}
-			temp=temp>>>1;
-			res=res<<1;
+			N = N >>> 1;
 		}
-        return (res);
-    }
+		sb = sb.reverse();
+		int res = 0;
+		for (int i = 0; i < sb.length(); i++) {
+			if (sb.charAt(i) == '0') {
+				res = res << 1;
+			} else if (sb.charAt(i) == '1') {
+				res = res << 1;
+				res = res | 1;
+			}
+		}
+		return (res);
+	}
 }
