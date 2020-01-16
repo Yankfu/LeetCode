a9717f8777c4b22aaa88cade88e58a58a9f8785a
Yankfu <857231252@qq.com>
2020-01-16 14:42:55 +0800

    再过几天我也得去河北了

--- _0289_生命游戏.java
+++ _0289_生命游戏.java
@@ -4,12 +4,27 @@ public class _0289_生命游戏 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[][] board = {{0,1,0},
-						{},
-						{},
-						{}};
+		int[][] board = {
+							{0,1,0},
+							{0,0,1},
+							{1,1,1},
+							{0,0,0}
+						};
+		gameOfLife(board);
+		for(int[] a:board) {
+			for(int b:a) {
+				System.out.print(b+" ");
+			}
+			System.out.println();
+		}
 	}
 	public static void gameOfLife(int[][] board) {
-        
+        for(int i=0;i<board.length;) {
+        	for(int j=0;j<board[0].length;j++) {
+        		if((i==0 && j==0) || (i==0 && j==board[0].length-1) || (i==board.length-1 && j==0) || (i==board.length-1 && j==board[0].length-1)) {
+        			
+        		}
+        	}
+        }
     }
 }
