71cf48774942671a2874fdcec59259d024de4ce4
Yankfu <857231252@qq.com>
2020-01-20 23:02:37 +0800

    出去吃了新辣道，感觉一般般

--- _test.java
+++ _test.java
@@ -1,13 +1,24 @@
 package com.LeetCode;
-
+import java.awt.*;
+import java.awt.event.InputEvent;
 public class _test {
 
-	public static void main(String[] args) {
-		if(1!=1 || 2!=2) {
-			System.out.println("||");
-		}
-		if(1!=1 && 2==2){
-			System.out.println("&&");
+	public static void main(String[] args) throws AWTException {
+		PointerInfo pinfo = MouseInfo.getPointerInfo();
+		Point p = pinfo.getLocation();
+		int x = (int) p.getX();
+		int y = (int) p.getY();
+		click(x,y);
+	}
+	public static void click(int x,int y) throws AWTException {
+		Robot r = new Robot();
+		r.delay(3000);
+		r.mouseMove(x, y);
+		for (int i = 0; i < 10000; i++) {
+			r.mousePress(InputEvent.BUTTON1_MASK);
+			r.delay(50);
+			r.mouseRelease(InputEvent.BUTTON1_MASK);
+			r.delay(300);
 		}
 	}
 }
