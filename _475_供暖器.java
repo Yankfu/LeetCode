d9969260abffd351dc5fa11ee59137f001ca1184
Yankfu <857231252@qq.com>
2020-01-22 10:31:38 +0800

    回家了

---
+++ _475_供暖器.java
@@ -0,0 +1,15 @@
+package com.LeetCode;
+
+public class _475_供暖器 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	//计算间隔除2
+	//比较大小，最大的既是最小半径
+	//记得添加头到第一个供暖期的距离和尾到最后一个供暖器的范围
+	public static int findRadius(int[] houses, int[] heaters) {
+        return 0;
+    }
+}
