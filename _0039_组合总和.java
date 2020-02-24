95cedfff6dd140b06c35163aee99c37c27204570
Yankfu <857231252@qq.com>
2020-02-24 19:01:17 +0800

    网课真刺激

--- _0039_组合总和.java
+++ _0039_组合总和.java
@@ -12,12 +12,15 @@ public class _0039_组合总和 {
 
     static List<List<Integer>> res = new ArrayList();
     static List<Integer> li = new ArrayList<>();
+
     public static List<List<Integer>> combinationSum(int[] candidates, int target) {
-        backTrack(candidates, target, 0,Integer.MIN_VALUE, li);
+        backTrack(candidates, target, 0, Integer.MIN_VALUE, li);
         return res;
     }
 
-    public static void backTrack(int[] candidates, int target, int now,int pre, List<Integer> li) {
+    //原数组，目标值，现在和，之前的值（用来剪枝），返回
+    public static void backTrack(int[] candidates, int target, int now, int pre, List<Integer> li) {
+        //如果等于目标值添加，返回
         if (now == target) {
 //            for (int i : li) {
 //                System.out.print(i + " ");
@@ -26,14 +29,14 @@ public class _0039_组合总和 {
             // Java 中可变对象是引用传递，因此需要将当前 path 里的值拷贝出来
             res.add(new ArrayList<>(li));
             return;
-        } else if (now > target) {
+        } else if (now > target) { //如果大于目标值直接返回
             return;
-        } else {
+        } else { //进行下一层的判断及回溯
             for (int i = 0; i < candidates.length; i++) {
-                if(pre<=candidates[i]){
-                    li.add(candidates[i]);
-                    backTrack(candidates, target, now + candidates[i],candidates[i], li);
-                    li.remove(li.size() - 1);
+                if (pre <= candidates[i]) { //如果现值小于之前的认为已经添加过了，进行剪枝避免重复计算
+                    li.add(candidates[i]); //添加到返回对象里
+                    backTrack(candidates, target, now + candidates[i], candidates[i], li);
+                    li.remove(li.size() - 1); //从返回对象里删除
                 }
             }
         }
