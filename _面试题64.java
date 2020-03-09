66e2f7e6fce0f7706ff74cd5710cb2ba31947cf1
Yankfu <857231252@qq.com>
2020-03-09 20:05:22 +0800

    睡了一天...

---
+++ _面试题64.java
@@ -0,0 +1,20 @@
+package com.LeetCode;
+
+/**
+ * @Author Yankfu
+ * @Email yankfu@foxmail.com
+ * @Date 2020/3/9 下午7:54
+ * @Version 1.0
+ */
+public class _面试题64 {
+	public static void main(String[] args) {
+		int n = 10;
+		System.out.println(sumNums(n));
+	}
+	//短路是真的牛批
+	public static int sumNums(int n) {
+		int res = n;
+		boolean b = (n>0) && ((res+=sumNums(n-1))>0);
+		return res;
+	}
+}
