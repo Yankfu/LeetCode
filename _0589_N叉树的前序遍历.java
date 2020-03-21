b626e39d98443bb065de3cc3b7da7c20d2f81832
Yankfu <857231252@qq.com>
2020-03-21 22:49:59 +0800

    三天一百级，升级真的快了好多

--- _0589_N叉树的前序遍历.java
+++ _0589_N叉树的前序遍历.java
@@ -27,22 +27,23 @@ public class _0589_N叉树的前序遍历 {
 			help(root.children.get(i));
 		}
 	}
+	class Node {
+		public int val;
+		public List<Node> children;
+
+		public Node() {}
+
+		public Node(int _val) {
+			val = _val;
+		}
+
+		public Node(int _val, List<Node> _children) {
+			val = _val;
+			children = _children;
+		}
+	};
 }
 /*
 // Definition for a Node.
-class Node {
-    public int val;
-    public List<Node> children;
 
-    public Node() {}
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-};
 */
