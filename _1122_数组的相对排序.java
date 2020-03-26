f42cf0f5afee500348ba42bdbeefee8ea54382cb
Yankfu <857231252@qq.com>
2020-03-26 23:14:32 +0800

    今天下了好大的雨啊。

--- _1122_数组的相对排序.java
+++ _1122_数组的相对排序.java
@@ -11,9 +11,24 @@ public class _1122_数组的相对排序 {
 	}
 
 	public static int[] relativeSortArray(int[] arr1, int[] arr2) {
-		
-		
-		
-		return arr1;
+		int[] count = new int[1001];
+		int[] res = new int[arr1.length];
+		for (int i = 0; i < arr1.length; i++) {
+			count[arr1[i]]++;
+		}
+		int index = 0;
+		for (int i = 0; i < arr2.length; i++) {
+			while (count[arr2[i]] > 0) {
+				res[index++] = arr2[i];
+				count[arr2[i]]--;
+			}
+		}
+		for (int i = 0; i < count.length; i++) {
+			while (count[i] > 0) {
+				res[index++] = i;
+				count[i]--;
+			}
+		}
+		return res;
     }
 }
