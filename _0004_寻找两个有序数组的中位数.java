800131d04d42580f1d49f6831576b1025afcc4a8
Yankfu <857231252@qq.com>
2020-04-07 21:56:44 +0800

    gangan单单

--- _0004_寻找两个有序数组的中位数.java
+++ _0004_寻找两个有序数组的中位数.java
@@ -5,79 +5,39 @@ public class _0004_寻找两个有序数组的中位数 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int[] a= {2};
-		int[] b= {1,3,4,5};
+		int[] a= {1,9,10};
+		int[] b= {2,4,5,6,8};
 		System.out.println(findMedianSortedArrays(a,b));
 	}
-
+	/*
+	把找中位数改成找前K个大的数字
+	如果是偶数个就找length/2 和 lenght/2+1个的
+	如果是奇数个就找length/2+1个
+	 */
 	public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-		int length=nums1.length+nums2.length;
-		int zhong = length/2;
-		int num1Point=0;
-		int num2Point=0;
-		int point = 0;
-		if(nums1.length==0 || (nums2.length!=0&&nums1[nums1.length-1]<=nums2[0])){
-			if(length%2 == 0){ //有两个中位数
-				if(nums1.length<zhong){
-					return (nums2[(zhong-nums1.length)]+nums2[(zhong-nums1.length)-1])/2.0;
-				}else if(nums1.length==zhong){
-					return (nums1[nums1.length-1]+nums2[0])/2.0;
-				}else{
-					return (nums1[zhong]+nums1[zhong-1])/2.0;
-				}
-			}else{
-				if(nums1.length<=zhong){
-					return nums2[(zhong-nums1.length)]/1.0;
-				}else{
-					return nums1[zhong]/1.0;
-				}
-			}
-		}else if(nums2.length == 0 || (nums1.length!=0&&nums1[0]>nums2[nums2.length-1])){
-			if(length%2 == 0){ //有两个中位数
-				if(nums2.length<zhong){
-					return (nums1[(zhong-nums2.length)]+nums1[(zhong-nums2.length)-1])/2.0;
-				}else if(nums2.length==zhong){
-					return (nums2[nums2.length-1]+nums1[0])/2.0;
-				}else{
-					return (nums2[zhong]+nums2[zhong-1])/2.0;
-				}
-			}else{
-				if(nums2.length<=zhong){
-					return nums1[(zhong-nums2.length)]/1.0;
-				}else{
-					return nums2[zhong]/1.0;
-				}
-			}
-		}else{
-			if(length%2 == 0){ //有两个中位数
-				while(point<length/2-1){
-					if(nums1[num1Point]>=nums2[num2Point]){
-						num2Point++;
-						point++;
-					}else if(nums1[num1Point]<nums2[num2Point]){
-						num1Point++;
-						point++;
-					}
-				}
-				if(nums1[num1Point]<nums2[num2Point] && num1Point+1<nums1.length){
-					return nums2[num2Point]>nums1[num1Point+1]?(nums1[num1Point]+nums1[num1Point+1])/2.0:(nums1[num1Point]+nums2[num2Point])/2.0;
-				}else if(nums1[num1Point]>nums2[num2Point]&& num2Point+1<nums2.length){
-					return nums1[num1Point]>nums2[num2Point+1]?(nums2[num2Point]+nums2[num2Point+1])/2.0:(nums2[num2Point]+nums1[num1Point])/2.0;
-				}else {
-					return (nums2[num2Point]+nums1[num1Point])/2.0;
-				}
-			}else{
-				while(point<length/2-1){
-					if(nums1[num1Point]>=nums2[num2Point]){
-						num2Point++;
-						point++;
-					}else if(nums1[num1Point]<nums2[num2Point]){
-						num1Point++;
-						point++;
-					}
-				}
-				return nums1[num1Point]>nums2[num2Point]?nums1[num1Point]/1.0:nums2[num2Point]/1.0;
-			}
+		int n = nums1.length;
+		int m = nums2.length;
+		int left = (n + m + 1) / 2;
+		int right = (n + m + 2) / 2;
+		//将偶数和奇数的情况合并，如果是奇数，会求两次同样的 k 。
+		return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;
+	}
+	private static int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+		int len1 = end1 - start1 + 1;
+		int len2 = end2 - start2 + 1;
+		//让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1
+		if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
+		if (len1 == 0) return nums2[start2 + k - 1];
+
+		if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+
+		int i = start1 + Math.min(len1, k / 2) - 1;
+		int j = start2 + Math.min(len2, k / 2) - 1;
+
+		if (nums1[i] > nums2[j]) {
+			return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
+		} else {
+			return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
 		}
 	}
 }
