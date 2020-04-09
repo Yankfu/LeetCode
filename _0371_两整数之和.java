dc5e88b29966f3773395375877493b4a33d15f94
Yankfu <857231252@qq.com>
2020-04-09 19:25:11 +0800

    我好像不应该选安卓

--- _0371_两整数之和.java
+++ _0371_两整数之和.java
@@ -6,19 +6,12 @@ public class _0371_两整数之和 {
 		// TODO Auto-generated method stub
 		System.out.println(((13&6)<<1));
 	}
-	public static int getSum(int a, int b) {
-        int res=0;
-        
-		return res;
-    }
+	public int getSum(int a, int b) {
+		while(b != 0){
+			int temp = a ^ b;
+			b = (a & b) << 1;
+			a = temp;
+		}
+		return a;
+	}
 }
-//class Solution {
-//    public int getSum(int a, int b) {
-//        while(b != 0){
-//            int temp = a ^ b;
-//            b = (a & b) << 1;
-//            a = temp;
-//        }
-//        return a;
-//    }
-//}
