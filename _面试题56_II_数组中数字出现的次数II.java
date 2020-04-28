697c878841c35b817148c3433e5b87ee895a4cb9
Yankfu <857231252@qq.com>
2020-04-28 21:47:46 +0800

    感觉香肠没以前好吃了

---
+++ _面试题56_II_数组中数字出现的次数II.java
@@ -0,0 +1,32 @@
+package com.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @PROJECT_NAME: 2020_春
+ * @DESCRIPTION:
+ * @USER: Yankfu
+ * @DATE: 2020/4/28 21:35
+ */
+public class _面试题56_II_数组中数字出现的次数II {
+    public static void main(String[] args) {
+
+    }
+    public  int singleNumber(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])){
+                map.put(nums[i],map.get(nums[i])+1);
+            }else {
+                map.put(nums[i],1);
+            }
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if (entry.getValue() == 1){
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
