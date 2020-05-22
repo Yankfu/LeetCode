1b8f31b5be1976910ea3af66bd76ea27a351c7be
Yankfu <857231252@qq.com>
2020-05-22 21:57:41 +0800

    其实悦悦穿裙子挺好看的

--- _面试题_17_04_消失的数字.java
+++ _面试题_17_04_消失的数字.java
@@ -8,24 +8,6 @@ package com.LeetCode;
  */
 public class _面试题_17_04_消失的数字 {
     public int missingNumber(int[] nums) {
-        boolean isBiggest = false;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > nums.length){
-                isBiggest = true;
-                continue;
-            }
-            nums[nums[i]] = -nums[Math.abs(nums[i])];
-        }
-        if (!isBiggest){
-            return nums.length+1;
-        }else {
-            for (int i = 0; i < nums.length; i++) {
-                if (nums[i] > 0){
-                    return i;
-                }
-            }
-        }
-
         int sum = nums.length;//记录实际总和
         for(int i = 0; i < nums.length; i++) {
             sum -= nums[i];//减去现在总和
