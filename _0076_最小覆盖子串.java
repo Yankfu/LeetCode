a196877af46df8513dda63b92059bbccaebb164b
Yankfu <857231252@qq.com>
2020-05-23 22:37:46 +0800

    加了块固态，真爽。

---
+++ _0076_最小覆盖子串.java
@@ -0,0 +1,63 @@
+package com.LeetCode;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+/**
+ * @PROJECT_NAME: 2020_春
+ * @DESCRIPTION:
+ * @USER: Yankfu
+ * @DATE: 2020/5/23 21:57
+ */
+public class _0076_最小覆盖子串 {
+    public static void main(String[] args) {
+        String S = "ADOBECODEBANC", T = "ABC";
+        System.out.println(minWindow(S,T));
+    }
+
+    static Map<Character, Integer> ori = new HashMap<Character, Integer>();
+    static Map<Character, Integer> cnt = new HashMap<Character, Integer>();
+
+    public static String minWindow(String s, String t) {
+        int tLen = t.length();
+        for (int i = 0; i < tLen; i++) {
+            char c = t.charAt(i);
+            ori.put(c, ori.getOrDefault(c, 0) + 1);
+        }
+        int l = 0, r = -1;
+        int len = Integer.MAX_VALUE, ansL = -1, ansR = -1;
+        int sLen = s.length();
+        while (r < sLen) {
+            ++r;
+            if (r < sLen && ori.containsKey(s.charAt(r))) {
+                cnt.put(s.charAt(r), cnt.getOrDefault(s.charAt(r), 0) + 1);
+            }
+            while (check() && l <= r) {
+                if (r - l + 1 < len) {
+                    len = r - l + 1;
+                    ansL = l;
+                    ansR = l + len;
+                }
+                if (ori.containsKey(s.charAt(l))) {
+                    cnt.put(s.charAt(l), cnt.getOrDefault(s.charAt(l), 0) - 1);
+                }
+                ++l;
+            }
+        }
+        return ansL == -1 ? "" : s.substring(ansL, ansR);
+    }
+
+    public static boolean check() {
+        Iterator iter = ori.entrySet().iterator();
+        while (iter.hasNext()) {
+            Map.Entry entry = (Map.Entry) iter.next();
+            Character key = (Character) entry.getKey();
+            Integer val = (Integer) entry.getValue();
+            if (cnt.getOrDefault(key, 0) < val) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
