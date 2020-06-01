588807c5bde46dbcfc7cd5a78e6194c45a2b335d
Yankfu <857231252@qq.com>
2020-06-01 21:14:50 +0800

    傻逼玩意

---
+++ _面试题_61_扑克牌中的顺子.java
@@ -0,0 +1,26 @@
+package com.LeetCode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @PROJECT_NAME: 2020_春
+ * @DESCRIPTION:
+ * @USER: Yankfu
+ * @DATE: 2020/6/1 21:04
+ */
+public class _面试题_61_扑克牌中的顺子 {
+    public boolean isStraight(int[] nums) {
+        Set<Integer> repeat = new HashSet<>();
+        int max = 0, min = 14;
+        for(int num : nums) {
+            if(num == 0) continue; // 跳过大小王
+            max = Math.max(max, num); // 最大牌
+            min = Math.min(min, num); // 最小牌
+            if(repeat.contains(num)) return false; // 若有重复，提前返回 false
+            repeat.add(num); // 添加此牌至 Set
+        }
+        return max - min < 5; // 最大牌 - 最小牌 < 5 则可构成顺子
+    }
+}
