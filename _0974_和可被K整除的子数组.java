8caa504d4ca614fb2418bacb5b008f65fe9efaa8
Yankfu <857231252@qq.com>
2020-06-02 21:43:24 +0800

    ...

---
+++ _0974_和可被K整除的子数组.java
@@ -0,0 +1,27 @@
+package com.LeetCode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @PROJECT_NAME: 2020_春
+ * @DESCRIPTION:
+ * @USER: Yankfu
+ * @DATE: 2020/5/27 22:23
+ */
+public class _0974_和可被K整除的子数组 {
+    public int subarraysDivByK(int[] A, int K) {
+        Map<Integer, Integer> record = new HashMap<>();
+        record.put(0, 1);
+        int sum = 0, ans = 0;
+        for (int elem: A) {
+            sum += elem;
+            // 注意 Java 取模的特殊性，当被除数为负数时取模结果为负数，需要纠正
+            int modulus = (sum % K + K) % K;
+            int same = record.getOrDefault(modulus, 0);
+            ans += same;
+            record.put(modulus, same + 1);
+        }
+        return ans;
+    }
+}
