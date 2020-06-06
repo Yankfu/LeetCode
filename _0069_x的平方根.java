26ff2c196e36a8aca4f0c36aa7fc5b99d3c5a726
Yankfu <857231252@qq.com>
2020-06-06 21:37:29 +0800

    今天又睡了一天

--- _0069_x的平方根.java
+++ _0069_x的平方根.java
@@ -4,8 +4,7 @@ public class _0069_x的平方根 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(mySqrt(Integer.MAX_VALUE));
-		System.out.println(Math.sqrt(Integer.MAX_VALUE));
+		System.out.println(mySqrt(15));
 	}
 	public static int mySqrt(int x) {
 		if(x==Integer.MAX_VALUE) {
---
+++ _0837_新21点.java
@@ -0,0 +1,31 @@
+package com.LeetCode;
+
+/**
+ * @PROJECT_NAME: 2020_春
+ * @DESCRIPTION:
+ * @USER: Yankfu
+ * @DATE: 2020/6/3 8:50
+ */
+public class _0837_新21点 {
+    public double new21Game(int N, int K, int W) {
+        // 先判断 K - 1 + W 是否在 N 的里面，如果在的话，说明肯定能赢得游戏，返回 1.0，也就是 100%
+        if (N - K + 1 >= W) {
+            return 1.0;
+        }
+        double[] dp = new double[K + W];
+        // 将能赢得游戏的点数的概率设置为 1
+        for (int i = K; i <= N; i++) {
+            dp[i] = 1.0;
+        }
+        // 计算K + W 这几个点数的概率和
+        double sumProb = N - K + 1;
+        // 从 K - 1 开始计算，
+        for (int i = K - 1; i >= 0; i--) {
+            // 点数为 i 的赢得游戏的概率为 i + 1 ~ i + W 的概率和除以 W
+            dp[i] = sumProb / W;
+            sumProb = sumProb - dp[i + W] + dp[i];
+        }
+
+        return dp[0];
+    }
+}
