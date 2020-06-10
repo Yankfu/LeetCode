7885bc11a6c8ff10c7f08ba8fa966222bf349e40
Yankfu <857231252@qq.com>
2020-06-10 12:59:10 +0800

    开始写

--- _0605_种花问题.java
+++ _0605_种花问题.java
@@ -4,18 +4,42 @@ public class _0605_种花问题 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int[] flowerbed = {1,0,0,0,1};
+		int n = 2;
+		System.out.println(canPlaceFlowers(flowerbed,n));
 	}
-	//统计所有已经种下的花
+
+	/**
+	 * 要统计的是含有三个0的问题
+	 *
+	 * @param flowerbed
+	 * @param n
+	 * @return
+	 */
 	public static boolean canPlaceFlowers(int[] flowerbed, int n) {
-        for(int i=0;i<flowerbed.length-3;i++) {
-        	if(flowerbed[i]==1) {
-        		
-        	}
-        	else if(flowerbed[i]==0 && flowerbed[i+1]==0 && flowerbed[i+2]==0) {
-        		
-        	}
-        }
+		int a = 0;
+		int b = 0;
+		int c = flowerbed[0];
+		for (int i = 1; i < flowerbed.length; i++) {
+			a = b;
+			b = c;
+			c = flowerbed[i];
+			//如果有这种空地就“种”一朵花
+			if (a == 0 && b == 0 && c == 0){
+				flowerbed[i-1] = 1;
+				b = 1;
+				n--;
+			}
+			if (n == 0){
+				return true;
+			}
+		}
+		if (b == 0 && c == 0){
+			n--;
+		}
+		if (n<=0){
+			return true;
+		}
 		return false;
     }
 }
