34810f1b67beb7021723c9665656a14f6ddc7a7c
Yankfu <857231252@qq.com>
2020-06-24 22:02:53 +0800

    明写linux，爱过不过

---
+++ _0581_最短无序连续子数组.java
@@ -0,0 +1,25 @@
+package com.LeetCode;
+
+import java.util.Arrays;
+
+/**
+ * @PROJECT_NAME: 2020_春
+ * @PACKAGE_NAME：com.LeetCode
+ * @DESCRIPTION:
+ * @AUTHOR: Yankfu
+ * @DATE: 2020/6/24 22:02
+ */
+public class _0581_最短无序连续子数组 {
+    public int findUnsortedSubarray(int[] nums) {
+        int[] snums = nums.clone();
+        Arrays.sort(snums);
+        int start = snums.length, end = 0;
+        for (int i = 0; i < snums.length; i++) {
+            if (snums[i] != nums[i]) {
+                start = Math.min(start, i);
+                end = Math.max(end, i);
+            }
+        }
+        return (end - start >= 0 ? end - start + 1 : 0);
+    }
+}
