28ee02b48defabdb5b72fc6b0c86dcb6cc59b779
Yankfu <yankfu@foxmail.com>
2022-02-25 00:48:25 +0800

    talk with parent and addd. Rebuild fils.

---
+++ No530To540/ComplexNumberMultiplication_537.java
@@ -0,0 +1,21 @@
+package No530To540;
+
+/**
+ * @PROJECT_NAME: LeetCode
+ * @PACKAGE_NAME：No530To540
+ * @DESCRIPTION: Perhaps, that has been the story of life
+ * MOD = 1000000007
+ * @AUTHOR: Yankfu
+ * @DATE: 2022/2/25 0:31
+ */
+public class ComplexNumberMultiplication_537 {
+    public static String complexNumberMultiply(String num1, String num2) {
+        String[] complex1 = num1.split("\\+|i");
+        String[] complex2 = num2.split("\\+|i");
+        int real1 = Integer.parseInt(complex1[0]);
+        int imag1 = Integer.parseInt(complex1[1]);
+        int real2 = Integer.parseInt(complex2[0]);
+        int imag2 = Integer.parseInt(complex2[1]);
+        return String.format("%d+%di", real1 * real2 - imag1 * imag2, real1 * imag2 + imag1 * real2);
+    }
+}
