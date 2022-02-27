46383e473440be527946feeb9e1803505c8ae622
Yankfu <yankfu@foxmail.com>
2022-02-28 00:25:07 +0800

    test new commit

--- no550To560/OptimalDivision_553.java
+++ no550To560/OptimalDivision_553.java
@@ -1,6 +1,11 @@
 package no550To560;
 
 public class OptimalDivision_553 {
+
+    public static void main(String[] args) {
+        System.out.println("add ssh to local");
+    }
+
     public String optimalDivision(int[] nums) {
         int n = nums.length;
         if (n == 1) {
